b573b26aa94cebad16483faeb3293f55c54bd879
raufnawazrtarar <33519174+raufnawazrtarar@users.noreply.github.com>
2019-11-12 14:21:55 +0000

    Created all the tables
    Still need to add values for all the tables except first one

--- sql/create.sql
+++ sql/create.sql
@@ -5,9 +5,9 @@
 # Version 1630
 # http://code.google.com/p/sequel-pro
 #
-# Host: 127.0.0.1 (MySQL 5.1.37)
-# Database: SQLTutorialMaster
-# Generation Time: 2010-01-14 14:44:38 +0000
+# Host: silva.computing.dundee.ac.uk (MySQL 5.1.37)
+# Database: 19ac3d05
+# Generation Time: 2019-12-14 12:44:38 +0000
 # ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,165 +19,159 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS `SQLTutorialMaster`;
+DROP DATABASE IF EXISTS `19ac3d05`;
 
-CREATE DATABASE `SQLTutorialMaster`;
+CREATE DATABASE `19ac3d05`;
 
-USE `SQLTutorialMaster`;
+USE `19ac3d05`;
 
-# Dump of table CARS
+# Dump of table Employees
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `CARS`;
+DROP TABLE IF EXISTS `Employees`;
 
-CREATE TABLE `CARS` (
-  `CarNo` int(11) NOT NULL,
-  `Make` varchar(15) DEFAULT NULL,
-  `Model` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`CarNo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `Employees` (
+  `Employee ID`  char(8) NOT NULL, 
+  Name           varchar(255) NOT NULL, 
+  Email          char(64) NOT NULL, 
+  Password       char(64) NOT NULL, 
+  Phone          char(14) NOT NULL, 
+  Salary         real NOT NULL, 
+  Role           varchar(255) NOT NULL, 
+  `Bank Details` int(255) NOT NULL, 
+  Picture        varchar(255), 
+  Building       char(8) NOT NULL, 
+  PRIMARY KEY (`Employee ID`));
+)
 
-LOCK TABLES `CARS` WRITE;
-/*!40000 ALTER TABLE `CARS` DISABLE KEYS */;
-INSERT INTO `CARS` (`CarNo`,`Make`,`Model`)
+LOCK TABLES `Employees` WRITE;
+/*!40000 ALTER TABLE `Employees` DISABLE KEYS */;
+INSERT INTO `Employees` (`Employee ID`, Name, Email, Phone, Salary, Role, `Bank Details`, Picture, Building)
 VALUES
-	(1,'Triumph','Spitfire'),
-	(2,'Bentley','Mk V'),
-	(3,'Triumph','Stag'),
-	(4,'Ford','GT 40'),
-	(5,'Shelby','Cobra'),
-	(6,'Ford','Mustang'),
-	(7,'Aston Martin','DB Mk III'),
-	(8,'Jaguar','D Type');
-
-/*!40000 ALTER TABLE `CARS` ENABLE KEYS */;
+	(123456,'Erick Nolan','Erick Nolan@skjervoy.com','07774696960','26000','Manager','00-22-11 10061011','link','Shop'),
+	(123457,'Peter Mayer','Peter Mayer@skjervoy.com','07774696961','27000','Manager','00-22-01 10061010','link','Shop'),
+	(123458,'Jamal Scott','Jamal Scott@skjervoy.com','07774696962','28000','Manager','00-22-12 10061012','link','Shop'),
+	(123459,'Keith Bates','Keith Bates@skjervoy.com','07774696963','26000','Manager','00-22-13 10061014','link','Shop'),
+	(123460,'Gregg Rivas','Gregg Rivas@skjervoy.com','07774696964','25500','Manager','00-22-14 10061013','link','Shop'),
+	(123461,'Ruby Wright','Ruby Wright@skjervoy.com','07774696965','25050','Manager','00-22-15 10061015','link','Shop'),
+	(123462,'Micah Ewing','Micah Ewing@skjervoy.com','07774696966','45000','Manager','00-22-16 10061016','link','Shop'),
+	(123463,'Maura Stone','Maura Stone@skjervoy.com','07774696967','75000','Manager','00-22-17 10061017','link','Shop'),
+  (123456,'Mitzi House','Mitzi House@skjervoy.com','07774696968','26000','Manager','00-22-18 10061018','link','Shop'),
+	(123457,'Steve Beard','Steve Beard@skjervoy.com','07774696969','20000','Manager','00-22-19 10061019','link','Shop'),
+	(123458,'Lloyd Riley','Lloyd Riley@skjervoy.com','07774696970','20000','Manager','00-22-21 10061020','link','Shop'),
+	(123459,'Isiah Rojas','Isiah Rojas@skjervoy.com','07774696971','29000','Manager','00-22-30 10061021','link','Shop'),
+	(123460,'Galen Cline','Galen Cline@skjervoy.com','07774696972','21000','Manager','00-22-31 10061023','link','Shop'),
+	(123461,'Davis Tyler','Davis Tyler@skjervoy.com','07774696973','29000','Manager','00-22-32 10061024','link','Shop'),
+	(123462,'Candy Mills','Candy Mills@skjervoy.com','07774696974','26000','Manager','00-22-33 10061025','link','Shop'),
+	(123463,'Vicky Johns','Vicky Johns@skjervoy.com','07774696975','26000','Manager','00-22-34 10061026','link','Shop'),
+  (123464,'Dixie Pitts','Dixie Pitts@skjervoy.com','07774696976','27000','Manager','00-22-35 10061027','link','Shop'),
+	(123465,'Deann Moran','Deann Moran@skjervoy.com','07774696977','29000','Manager','00-22-36 10061028','link','Shop'),
+	(123466,'Essie Oneal','Essie Oneal@skjervoy.com','07774696978','29000','Manager','00-22-37 10061029','link','Shop'),
+	(123467,'Alice Doyle','Alice Doyle@skjervoy.com','07774696979','24000','Manager','00-22-16 10061030','link','Shop'),
+	(123468,'Ricky Eaton','Ricky Eaton@skjervoy.com','07774696980','23000','Manager','00-22-99 10061031','link','Shop'),
+	(123469,'Ilene Bruce','Ilene Bruce@skjervoy.com','07774696981','21000','Manager','00-22-44 10061032','link','Shop'),
+	(123470,'David Avery','David Avery@skjervoy.com','07774696982','22000','Manager','00-22-66 10061033','link','Shop'),
+	(123471,'Allan Ellis','Allan Ellis@skjervoy.com','07774696983','29000','Manager','00-22-77 10061034','link','Shop'),
+/*!40000 ALTER TABLE `Employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table EMPLOYEES
+# Dump of table Buildings
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `EMPLOYEES`;
+DROP TABLE IF EXISTS `Buildings`;
 
-CREATE TABLE `EMPLOYEES` (
-  `EmployeeNo` int(11) NOT NULL,
-  `FirstName` varchar(10) DEFAULT NULL,
-  `LastName` varchar(12) DEFAULT NULL,
-  `DateOfBirth` date DEFAULT NULL,
-  `DateEmployed` date DEFAULT NULL,
-  `CarNo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`EmployeeNo`),
-  KEY `fk_Employees_Cars1` (`CarNo`),
-  CONSTRAINT `fk_Employees_Cars1` FOREIGN KEY (`CarNo`) REFERENCES `CARS` (`CarNo`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `Buildings` (
+  `Building ID`  char(8) NOT NULL, 
+  Type           varchar(16) NOT NULL, 
+  `Phone Number` char(14), 
+  Manager        char(8) NOT NULL, 
+  Address        char(16) NOT NULL, 
+  Revenue        real, 
+  PRIMARY KEY (`Building ID`));
+) 
 
-LOCK TABLES `EMPLOYEES` WRITE;
-/*!40000 ALTER TABLE `EMPLOYEES` DISABLE KEYS */;
-INSERT INTO `EMPLOYEES` (`EmployeeNo`,`FirstName`,`LastName`,`DateOfBirth`,`DateEmployed`,`CarNo`)
+LOCK TABLES `Buildings` WRITE;
+/*!40000 ALTER TABLE `Buildings` DISABLE KEYS */;
+INSERT INTO `Buildings` (`Building ID`,Type,`Phone Number`,Manager,Address,Revenue)
 VALUES
-	(1,'Bilda','Groves','1966-04-01','1999-05-01',2),
-	(2,'John','Greeves','1977-03-21','2000-01-01',NULL),
-	(3,'Sally','Smith','1977-05-01','2002-04-01',5),
-	(4,'Fred','Jones','1996-04-03','2004-05-01',3);
+	(1,'Bilda','Groves','1966-04-01','1999-05-01',2);
 
-/*!40000 ALTER TABLE `EMPLOYEES` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Buildings` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table SALES
+# Dump of table Inventory
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `SALES`;
+DROP TABLE IF EXISTS `Inventory`;
 
-CREATE TABLE `SALES` (
-  `SalesNo` int(11) NOT NULL,
-  `EmployeeNo` int(11) DEFAULT NULL,
-  `Customer` varchar(12) DEFAULT NULL,
-  `Item` varchar(10) DEFAULT NULL,
-  `Supplier` varchar(12) DEFAULT NULL,
-  `Amount` decimal(6,2) DEFAULT NULL,
-  PRIMARY KEY (`SalesNo`),
-  KEY `fk_Sales_Employees1` (`EmployeeNo`),
-  CONSTRAINT `fk_Sales_Employees1` FOREIGN KEY (`EmployeeNo`) REFERENCES `EMPLOYEES` (`EmployeeNo`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `Inventory` (
+`Inventory Entry ID`        char(16) NOT NULL, 
+`Product ID`                char(16) NOT NULL, 
+ Quantity                    int(1) NOT NULL, 
+ Building                    char(8) NOT NULL, 
+ `Minimum Required Quantity` int(1) NOT NULL, 
+ PRIMARY KEY (`Inventory Entry ID`));
+) 
 
-LOCK TABLES `SALES` WRITE;
+LOCK TABLES `Inventory` WRITE;
 /*!40000 ALTER TABLE `SALES` DISABLE KEYS */;
-INSERT INTO `SALES` (`SalesNo`,`EmployeeNo`,`Customer`,`Item`,`Supplier`,`Amount`)
+INSERT INTO `Inventory` (`Inventory Entry ID`,`Product ID` ,Quantity, Building,`Minimum Required Quantity`)
 VALUES
-	(1,1,'Simpson','Sofa','Harrison',235.67),
-	(2,1,'Johnson','Chair','Harrison',453.78),
-	(3,2,'Smith','Stool','Ford',82.78),
-	(4,2,'Jones','Suite','Harrison',3421.00),
-	(5,3,'Smith','Sofa','Harrison',235.67),
-	(6,1,'Simposn','Sofa','Harrison',235.67),
-	(7,1,'Jones','Bed','Ford',453.00);
+	(1,1,'Simpson','Sofa','Harrison',235.67);
 
-/*!40000 ALTER TABLE `SALES` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table SALES2
+# Dump of table Products
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `SALES2`;
+DROP TABLE IF EXISTS `Products`;
 
-CREATE TABLE `SALES2` (
-  `SalesNo` int(11) NOT NULL,
-  `EmployeeNo` int(11) DEFAULT NULL,
-  `Customer` varchar(12) DEFAULT NULL,
-  `Item` varchar(10) DEFAULT NULL,
-  `Supplier` varchar(12) DEFAULT NULL,
-  `Amount` decimal(6,2) DEFAULT NULL,
-  PRIMARY KEY (`SalesNo`),
-  KEY `fk_Sales2_Employees1` (`EmployeeNo`),
-  CONSTRAINT `fk_Sales2_Employees1` FOREIGN KEY (`EmployeeNo`) REFERENCES `EMPLOYEES` (`EmployeeNo`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `Products` (
+  `Product ID`    char(16) NOT NULL, 
+  Name            varchar(255) NOT NULL, 
+  Type            char(64) NOT NULL, 
+  `Buying Price`  real NOT NULL, 
+  `Selling Price` real NOT NULL, 
+  Weight          real NOT NULL, 
+  Picture         varchar(255), 
+  Supplier        varchar(255) NOT NULL, 
+  PRIMARY KEY (`Product ID`));
+) 
 
-LOCK TABLES `SALES2` WRITE;
-/*!40000 ALTER TABLE `SALES2` DISABLE KEYS */;
-INSERT INTO `SALES2` (`SalesNo`,`EmployeeNo`,`Customer`,`Item`,`Supplier`,`Amount`)
+LOCK TABLES `Products` WRITE;
+/*!40000 ALTER TABLE `Products` DISABLE KEYS */;
+INSERT INTO `Products` (`Product ID`,Name,Type,`Buying Price`,`Selling Price`,Weight, Picture, Supplier)
 VALUES
-	(3,2,'Smith','Stool','Ford',82.78),
-	(5,3,'Smith','Sofa','Harrison',235.67),
-	(213,3,'Willaims','Suite','Harrison',3421.00),
-	(216,2,'McGreggor','Bed','Ford',453.00),
-	(217,1,'Williams','Sofa','Harrison',235.67),
-	(218,3,'Aitken','Sofa','Harrison',235.67),
-	(225,2,'Aitken','Chair','Harrison',453.78);
+	(3,2,'Smith','Stool','Ford',82.78);
 
-/*!40000 ALTER TABLE `SALES2` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Products` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table SALESPEOPLE
+# Dump of table Couriers
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `SALESPEOPLE`;
+DROP TABLE IF EXISTS `Couriers`;
 
-CREATE TABLE `SALESPEOPLE` (
-  `EmployeeNo` int(11) NOT NULL,
-  `FirstName` varchar(12) DEFAULT NULL,
-  `LastName` varchar(12) DEFAULT NULL,
-  `CarNo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`EmployeeNo`),
-  KEY `fk_SalesPeople_Cars1` (`CarNo`),
-  CONSTRAINT `fk_SalesPeople_Cars1` FOREIGN KEY (`CarNo`) REFERENCES `CARS` (`CarNo`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `Couriers` (
+ Name               varchar(255) NOT NULL, 
+ Address            char(16) NOT NULL, 
+ `Cost per Kg`      real NOT NULL, 
+ `Collection point` char(16) NOT NULL, 
+ `Warehouse ID`     char(8) NOT NULL, 
+  PRIMARY KEY (Name));
 
-LOCK TABLES `SALESPEOPLE` WRITE;
-/*!40000 ALTER TABLE `SALESPEOPLE` DISABLE KEYS */;
-INSERT INTO `SALESPEOPLE` (`EmployeeNo`,`FirstName`,`LastName`,`CarNo`)
+LOCK TABLES `Couriers` WRITE;
+/*!40000 ALTER TABLE `Couriers` DISABLE KEYS */;
+INSERT INTO `Couriers` (Name, Address, `Cost per Kg`, `Collection point`, `Warehouse ID`)
 VALUES
-	(1,'Fred','Williams',1),
-	(2,'Sarah','Watson',4),
-	(3,'James','Hatlitch',6),
-	(4,'Simon','Webaston',NULL),
-	(5,'Sally','Harcourt',NULL),
-	(6,'Martin','Boxer',NULL),
-	(7,'Trevor','Wright',7);
+	(1,'Fred','Williams',1);
 
-/*!40000 ALTER TABLE `SALESPEOPLE` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Couriers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 

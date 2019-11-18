8257238730dcb32ac4b017d5e1fffbf2299b86d7
fpavlica <33609416+fpavlica@users.noreply.github.com>
2019-11-18 12:16:21 +0000

    added employee picture paths

--- sql/create.sql
+++ sql/create.sql
@@ -41,30 +41,20 @@ LOCK TABLES `Employees` WRITE;
 /*!40000 ALTER TABLE `Employees` DISABLE KEYS */;
 INSERT INTO `Employees` (`Employee ID`, Name, Email, Phone, Salary, Role, `Bank Details`, Picture, Building, Password) # pw is just 'password'
 VALUES
-	('00123448','Erick Nolan','ErickNolan@skjervoy.com','07774696960',26000,'Product Manager','00-22-11 10061011',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123449','Peter Mayer','PeterMayer@skjervoy.com','07774696961',27000,'HR Manager','00-22-01 10061010','/img/emp/peter_mayer.jpg','00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123450','Jamal Scott','JamalScott@skjervoy.com','07774696962',28000,'Manager','00-22-12 10061012',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123451','Keith Bates','KeithBates@skjervoy.com','07774696963',26000,'Manager','00-22-13 10061014',null,'00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), # shop mgr
+	('00123448','Erick Nolan','ErickNolan@skjervoy.com','07774696960',26000,'Product Manager','00-22-11 10061011','img/emp/erick_nolan.jpg','00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123449','Peter Mayer','PeterMayer@skjervoy.com','07774696961',27000,'HR Manager','00-22-01 10061010','img/emp/peter_mayer.jpg','00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123450','Jamal Scott','JamalScott@skjervoy.com','07774696962',28000,'Manager','00-22-12 10061012','img/emp/jamal_scott.jpg','00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123451','Keith Bates','KeithBates@skjervoy.com','07774696963',26000,'Manager','00-22-13 10061014','img/emp/keith_bates.jpg','00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), # shop mgr
 	('00123452','Gregg Rivas','GreggRivas@skjervoy.com','07774696964',25500,'Manager','00-22-14 10061013',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123453','Ruby Wright','RubyWright@skjervoy.com','07774696965',25050,'Manager','00-22-15 10061015',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123454','Micah Ewing','MicahEwing@skjervoy.com','07774696966',45000,'Manager','00-22-16 10061016',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123455','Maura Stone','MauraStone@skjervoy.com','07774696967',75000,'Sales Assistant','00-22-17 10061017',null,'00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123455','Maura Stone','MauraStone@skjervoy.com','07774696967',75000,'Sales Assistant','00-22-17 10061017','img/emp/maura_stone.jpg','00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
   ('00123456','Mitzi House','MitziHouse@skjervoy.com','07774696968',26000,'Sales Assistant','00-22-18 10061018',null,'00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123457','Steve Beard','SteveBeard@skjervoy.com','07774696969',20000,'Sales Assistant','00-22-19 10061019',null,'00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123458','Lloyd Riley','LloydRiley@skjervoy.com','07774696970',20000,'Sales Assistant','00-22-21 10061020',null,'00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123459','Isiah Rojas','IsiahRojas@skjervoy.com','07774696971',29000,'Manager','00-22-30 10061021',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123460','Galen Cline','GalenCline@skjervoy.com','07774696972',21000,'Manager','00-22-31 10061023',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123461','Davis Tyler','DavisTyler@skjervoy.com','07774696973',29000,'Manager','00-22-32 10061024',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123462','Candy Mills','CandyMills@skjervoy.com','07774696974',26000,'Manager','00-22-33 10061025',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123463','Vicky Johns','VickyJohns@skjervoy.com','07774696975',26000,'Manager','00-22-34 10061026',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-  ('00123464','Dixie Pitts','DixiePitts@skjervoy.com','07774696976',27000,'Manager','00-22-35 10061027',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123465','Deann Moran','DeannMoran@skjervoy.com','07774696977',29000,'Manager','00-22-36 10061028',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123466','Essie Oneal','EssieOneal@skjervoy.com','07774696978',29000,'Manager','00-22-37 10061029',null,'00111111', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), # wh mgr
-	('00123467','Alice Doyle','AliceDoyle@skjervoy.com','07774696979',24000,'Warehouse Assistant','00-22-16 10061030',null,'00111111', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123468','Ricky Eaton','RickyEaton@skjervoy.com','07774696980',23000,'Warehouse Assistant','00-22-99 10061031',null,'00111111', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123469','Ilene Bruce','IleneBruce@skjervoy.com','07774696981',21000,'Manager','00-22-44 10061032',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-	('00123470','David Avery','DavidAvery@skjervoy.com','07774696982',22000,'Manager','00-22-66 10061033',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), #office mgr
-	('00123471','Allan Ellis','AllanEllis@skjervoy.com','07774696983',29000,'Manager','00-22-77 10061034',null,'00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
+	('00123457','Steve Beard','SteveBeard@skjervoy.com','07774696969',20000,'Sales Assistant','00-22-19 10061019','img/emp/steve_beard.jpg','00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123458','Lloyd Riley','LloydRiley@skjervoy.com','07774696970',20000,'Sales Assistant','00-22-21 10061020','img/emp/lloyd_riley.jpg','00333333', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123466','Essie Oneal','EssieOneal@skjervoy.com','07774696978',29000,'Manager','00-22-37 10061029','img/emp/essie_oneal.jpg','00111111', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), # wh mgr
+	('00123467','Alice Doyle','AliceDoyle@skjervoy.com','07774696979',24000,'Warehouse Assistant','00-22-16 10061030','img/emp/alice_doyle.jpg','00111111', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123468','Ricky Eaton','RickyEaton@skjervoy.com','07774696980',23000,'Warehouse Assistant','00-22-99 10061031','img/emp/ricky_eaton.jpg','00111111', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+	('00123470','David Avery','DavidAvery@skjervoy.com','07774696982',22000,'Manager','00-22-66 10061033','img/emp/david_avery.jpg','00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), #office mgr
+	('00123471','Allan Ellis','AllanEllis@skjervoy.com','07774696983',29000,'Manager','00-22-77 10061034','img/emp/allan_ellis.jpg','00222222', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
 /*!40000 ALTER TABLE `Employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,48 +123,46 @@ CREATE TABLE `Products` (
   `Selling Price` real NOT NULL, 
   Weight          real NOT NULL, 
   Picture         varchar(255), 
-  Supplier        char(16) NOT NULL, 
-  Series          varchar(255) NOT NULL,
+  Supplier        varchar(255) NOT NULL, 
+  Series          char(16) NOT NULL,
   PRIMARY KEY (`Product ID`));
 
 LOCK TABLES `Products` WRITE;
 /*!40000 ALTER TABLE `Products` DISABLE KEYS */;
 INSERT INTO `Products` (`Product ID`,Name,Type,`Buying Price`,`Selling Price`,Weight, Picture, Supplier, Series)
 VALUES 
-	('3000111122223333','Fargerik', 'Pen','10.99','99.99','1','img/pen/fargerik.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223334','Glatt', 'Pen','10.99','199.99','1','img/pen/glatt.jpg','0000000000000003', 'Elite'),
-  ('3000111122223335','Tre', 'Pen','10.99','9.99','1','img/pen/tre.jpg','0000000000000003', 'Fire'),
-  ('3000111122223336','Levende', 'Pen','10.99','59.99','1','img/pen/levende.jpg','0000000000000003', 'Elite'),
-  ('3000111122223337','Titan', 'Pen','10.99','49.99','1','img/pen/titan.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223338','Sukker', 'Pen','10.99','999.99','1','img/pen/sukker.jpg','0000000000000003', 'Fire'),
-  ('3000111122223339','Energi', 'Pen','10.99','69.69','1','img/pen/energi.jpg','0000000000000003', 'Elite'),
-  ('3000111122223340','Syndiket', 'Pen','10.99','420','1','img/pen/syndikat.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223341','Halogen', 'Pen','10.99','99.99','1','img/pen/halogen.jpg','0000000000000003', 'Elite'),
-  ('3000111122223342','Vestlig', 'Pen','10.99','99.99','1','img/pen/vestlig.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223343','Brann', 'Pen','10.99','129.99','1','img/pen/brann.jpg','0000000000000003', 'Fire'),
-  ('3000111122223344','Sunn', 'Pen','10.99','89.99','1','img/pen/sunn.jpg','0000000000000003', 'Elite'),
-  ('3000111122223345','Metallutstyr', 'Pen','5.99','18.99','1','img/pen/metallutstyr.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223346','Marken', 'Pen','10.99','82.99','1','img/pen/marken.jpg','0000000000000003', 'Elite'),
-  ('3000111122223347','Luksus', 'Pen','10.99','99.99','1','img/pen/luksus.jpg','0000000000000003', 'Fire'),
-  
-  ('3000111122223348','Vitaminer', 'Notebook','1.99','9.99','1','img/nb/vitaminer.jpg','0000000000000003', 'Fjord'),
-  ('3000111122223349','Utholdenhet', 'Notebook','1.99','19.99','1','img/nb/utholdenhet.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223350','Gulrot', 'Notebook','1.99','19.99','1','img/nb/gulrot.jpg','0000000000000003', 'Elite'),
-  ('3000111122223351','Rockestjerne', 'Notebook','1.99','9.99','1','img/nb/rockestjerne.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223352','Eleganse', 'Notebook','1.99','8.99','1','img/nb/eleganse.jpg','0000000000000003', 'Elite'),
-  ('3000111122223353','Forløsning', 'Notebook','1.99','7.99','1','img/nb/forlosning.jpg','0000000000000003', 'Fjord'),
-  ('3000111122223354','Nødvendig', 'Notebook','1.99','6.99','1','img/nb/nodvendig.jpg','0000000000000003', 'Elite'),
-  ('3000111122223355','Løve', 'Notebook','1.99','9.99','1','img/nb/love.jpg','0000000000000003', 'Fjord'),
-  ('3000111122223356','Kurer', 'Notebook','1.99','11.99','1','img/nb/kurer.jpg','0000000000000003', 'Elite'),
-  ('3000111122223357','Skapning', 'Notebook','1.99','12.99','1','img/nb/skapning.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223358','Korn', 'Notebook','1.99','8.99','1','img/nb/korn.jpg','0000000000000003', 'Elite'),
-  ('3000111122223359','Moro', 'Notebook','1.99','7.99','1','img/nb/moro.jpg','0000000000000003', 'Fjord'),
-  ('3000111122223360','Kalibrering', 'Notebook','1.99','9.99','1','img/nb/kalibrering.jpg','0000000000000003', 'Elite'),
-  ('3000111122223361','Snikmorder', 'Notebook','0.99','8.99','1','img/nb/snikmorder.jpg','0000000000000003', 'Excellence'),
-  ('3000111122223362','Vann', 'Notebook','1.99','4.99','1','img/nb/vann.jpg','0000000000000003', 'Elite'),
-	       
-  ('3000111122223363','Safari', 'Pen','16.99','28.99','1','img/pen/safari.jpg','0000000000000001', 'Other'),
-  ('3500111122223333','Metropolitan', 'Pen','12.99','21.99','1','img/pen/metropolitan.jpg','0000000000000002', 'Other')
+	('3000111122223333','Fargerik', 'Pen','10.99','99.99','1','/img/pen/fargerik.jpg','0000000000000003', 'Excellence'),
+  ('3000111122223334','Glatt', 'Pen','10.99','99.99','1','/img/pen/glatt.jpg','0000000000000003', 'Elite'),
+  ('3000111122223335','Tre', 'Pen','10.99','99.99','1','/img/pen/tre.jpg','0000000000000003', 'Fire'),
+  ('3000111122223336','Levende', 'Pen','10.99','99.99','1','/img/pen/levende.jpg','0000000000000003', 'Elite'),
+  ('3000111122223337','Titan', 'Pen','10.99','99.99','1','/img/pen/titan.jpg','0000000000000003', 'Elite'),
+  ('3000111122223338','Sukker', 'Pen','10.99','99.99','1','/img/pen/sukker.jpg','0000000000000003', 'Elite'),
+  ('3000111122223339','Energi', 'Pen','10.99','99.99','1','/img/pen/energi.jpg','0000000000000003', 'Elite'),
+  ('3000111122223340','Syndiket', 'Pen','10.99','99.99','1','/img/pen/syndikat.jpg','0000000000000003', 'Elite'),
+  ('3000111122223341','Halogen', 'Pen','10.99','99.99','1','/img/pen/halogen.jpg','0000000000000003', 'Elite'),
+  ('3000111122223342','Vestlig', 'Pen','10.99','99.99','1','/img/pen/vestlig.jpg','0000000000000003', 'Elite'),
+  ('3000111122223343','Brann', 'Pen','10.99','99.99','1','/img/pen/brann.jpg','0000000000000003', 'Elite'),
+  ('3000111122223344','Sunn', 'Pen','10.99','99.99','1','/img/pen/sunn.jpg','0000000000000003', 'Elite'),
+  ('3000111122223345','Metallutstyr', 'Pen','10.99','99.99','1','/img/pen/metallutstyr.jpg','0000000000000003', 'Elite'),
+  ('3000111122223346','Marken', 'Pen','10.99','99.99','1','/img/pen/marken.jpg','0000000000000003', 'Elite'),
+  ('3000111122223347','Luksus', 'Pen','10.99','99.99','1','/img/pen/luksus.jpg','0000000000000003', 'Elite'),
+  ('3000111122223348','Vitaminer', 'Notebook','10.99','99.99','1','/img/pen/vitaminer.jpg','0000000000000003', 'Elite'),
+  ('3000111122223349','Utholdenhet', 'Notebook','10.99','99.99','1','/img/pen/utholdenhet.jpg','0000000000000003', 'Elite'),
+  ('3000111122223350','Gulrot', 'Notebook','10.99','99.99','1','/img/pen/gulrot.jpg','0000000000000003', 'Elite'),
+  ('3000111122223351','Rockestjerne', 'Notebook','10.99','99.99','1','/img/pen/rockestjerne.jpg','0000000000000003', 'Elite'),
+  ('3000111122223352','Eleganse', 'Notebook','10.99','99.99','1','/img/pen/eleganse.jpg','0000000000000003', 'Elite'),
+  ('3000111122223353','Forløsning', 'Notebook','10.99','99.99','1','/img/pen/forlosning.jpg','0000000000000003', 'Elite'),
+  ('3000111122223354','Nødvendig', 'Notebook','10.99','99.99','1','/img/pen/nodvendig.jpg','0000000000000003', 'Elite'),
+  ('3000111122223355','Løve', 'Notebook','10.99','99.99','1','/img/pen/love.jpg','0000000000000003', 'Elite'),
+  ('3000111122223356','Kurer', 'Notebook','10.99','99.99','1','/img/pen/kurer.jpg','0000000000000003', 'Elite'),
+  ('3000111122223357','Skapning', 'Notebook','10.99','99.99','1','/img/pen/skapning.jpg','0000000000000003', 'Elite'),
+  ('3000111122223358','Korn', 'Notebook','10.99','99.99','1','/img/pen/korn.jpg','0000000000000003', 'Elite'),
+  ('3000111122223359','Moro', 'Notebook','10.99','99.99','1','/img/pen/moro.jpg','0000000000000003', 'Elite'),
+  ('3000111122223360','Kalibrering', 'Notebook','10.99','99.99','1','/img/pen/kalibrering.jpg','0000000000000003', 'Elite'),
+  ('3000111122223361','Snikmorder', 'Notebook','10.99','99.99','1','/img/pen/snikmorder.jpg','0000000000000003', 'Elite'),
+  ('3000111122223362','Vann', 'Notebook','10.99','99.99','1','/img/pen/vann.jpg','0000000000000003', 'Elite'),
+  ('3000111122223363','Safari', 'Pen','10.99','99.99','1','/img/pen/vann.jpg','0000000000000001', 'Other'),
+  ('3500111122223333','Metropolitan', 'Pen','10.99','99.99','1','/img/pen/vann.jpg','0000000000000002', 'Other')
   ;
 
 /*!40000 ALTER TABLE `Products` ENABLE KEYS */;
